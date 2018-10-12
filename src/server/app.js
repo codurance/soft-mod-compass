42e2b6607ee9b5ad89d4e61ea3cda34111ea105b
Daniel Parkin <danielparkin1@gmail.com>
2018-10-12 16:17:11 +0100

    feat: add plumbing - get answers for given submission

--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const rp = require('request-promise');
 const express = require('express')
 const app = express()
 
@@ -11,7 +12,28 @@ app.get('/', (req, res) => {
 })
 
 app.get('/results', (req, res) => {
-  let id = req.query.id
+  const id = req.query.id
+
+  const options = {
+    uri: `https://api.typeform.com/forms/Lks1RA/responses`,
+    qs: {
+        query: id
+    },
+    headers: {
+        'Authorization': 'Bearer A4ertXpQ7ieS26cUM5H1odoeaBR8NTnMSGCkSDKPcsNZ'
+    },
+    json: true
+  };
+
+  rp(options)
+    .then(function (response) {
+      const answers = response.items[0].answers
+      console.log(answers);
+    })
+    .catch(function (err) {
+      console.log('oh snap!')
+    });
+
   res.render('results', { data: [40, 10, 50, 30, 70, 40]});
 })
 
