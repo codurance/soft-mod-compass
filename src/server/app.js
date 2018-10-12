aa2b86e072e3f6103d04c8cec0c99e11f017445c
Daniel Parkin <danielparkin1@gmail.com>
2018-10-12 16:31:38 +0100

    refactor: extract variables and remove semicolons

--- src/server/app.js
+++ src/server/app.js
@@ -1,26 +1,28 @@
 const path = require('path')
-const rp = require('request-promise');
+const rp = require('request-promise')
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'))
 app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index')
 })
 
 app.get('/results', (req, res) => {
-  const id = req.query.id
+  const respondentId = req.query.id
+  const formId = 'Lks1RA'
+  const authToken = 'A4ertXpQ7ieS26cUM5H1odoeaBR8NTnMSGCkSDKPcsNZ'
 
   const options = {
-    uri: `https://api.typeform.com/forms/Lks1RA/responses`,
+    uri: `https://api.typeform.com/forms/${formId}/responses`,
     qs: {
-        query: id
+        query: respondentId
     },
     headers: {
-        'Authorization': 'Bearer A4ertXpQ7ieS26cUM5H1odoeaBR8NTnMSGCkSDKPcsNZ'
+        'Authorization': `Bearer ${authToken}`
     },
     json: true
   };
@@ -34,7 +36,7 @@ app.get('/results', (req, res) => {
       console.log('oh snap!')
     });
 
-  res.render('results', { data: [40, 10, 50, 30, 70, 40]});
+  res.render('results', { data: [40, 10, 50, 30, 70, 40]})
 })
 
 module.exports = app
