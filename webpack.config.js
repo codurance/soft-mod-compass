695ce9a0ed08be2feb6f20e7ab67f3baf1100d88
Liam Griffin <gryffskin@gmail.com>
2018-11-06 11:36:52 +0000

    Add watch script for local development

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   entry: './src/client/index.js',
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
