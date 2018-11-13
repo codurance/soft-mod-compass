e81b6025c486a398ee828eca6c69384bd4a0ce5f
Liam Griffin <gryffskin@gmail.com>
2018-11-13 11:30:43 +0000

    Remove unused routes

--- src/server/app.js
+++ src/server/app.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const rp = require('request-promise')
 const jsreport = require('jsreport')
 const express = require('express')
 const stripHubspotSubmissionGuid = require('./middleware/stripHubspotSubmissionGuid')
@@ -15,34 +14,6 @@ app.use(express.static('dist'))
 
 app.get('/', (req, res) => {
   res.render('index')
-})
-
-app.get('/results', (req, res) => {
-  const respondentId = req.query.id
-  const formId = 'yiRLeY'
-  const authToken = '3U8FHS7YZV4GCpbwyxNUybKaAQAQZAzFyXoqCFeGqYRk'
-
-  const options = {
-    uri: `https://api.typeform.com/forms/${formId}/responses`,
-    qs: {
-      query: respondentId
-    },
-    headers: {
-      'Authorization': `Bearer ${authToken}`
-    },
-    json: true
-  }
-
-  rp(options)
-    .then(function (response) {
-      const answers = response.items[0].answers
-      console.log(answers)
-    })
-    .catch(function () {
-      console.log('oh snap!')
-    })
-
-  res.render('results', { data: [40, 10, 50, 30, 70, 40] })
 })
 
 app.get('/report/:uuid', (req, res) => {
@@ -68,16 +39,6 @@ app.get('/report/:uuid', (req, res) => {
       res.sendStatus(500)
     })
 })
-
-app.get('/test-data', (req, res) => {
-  res.json({
-    scores: randomScores()
-  })
-})
-
-function randomScores () {
-  return [...Array(5)].map(i => Math.round(Math.random() * 100))
-}
 
 function categoryData () {
   return [
