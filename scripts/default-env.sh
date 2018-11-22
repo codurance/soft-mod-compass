719f7095df7611ef38dbc07a4d2d995255f31d5d
Dan Parkin <daniel.parkin@codurance.com>
2018-11-22 15:21:15 +0000

    chore: remove default-env config

--- scripts/default-env.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export TYPEFORM_URL="https://mashooqbadar.typeform.com"
-export TYPEFORM_FORM_ID="yiRLeY"
-export TYPEFORM_AUTH_TOKEN="3U8FHS7YZV4GCpbwyxNUybKaAQAQZAzFyXoqCFeGqYRk"
