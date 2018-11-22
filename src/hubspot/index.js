4d30c611a1201e165f32b1a7265648ab890aa4b1
Dan Parkin <daniel.parkin@codurance.com>
2018-11-22 16:47:47 +0000

    chore: test access environment variable from secret store

--- src/hubspot/index.js
+++ src/hubspot/index.js
@@ -4,7 +4,8 @@ const readFile = require('./readFile')
 
 const startDeployment = async () => {
   const builtLandingPage = build(readFile)
-  console.log(process.env.HUBSPOT_AUTH_TOKEN, 'hubspot auth')
+  console.log(process.env.HUBSPOT_AUTH_TOKEN, 'hubspot')
+  console.log(process.env, 'env vars')
   await deploy('256e3ebf-f076-4016-a6c9-c787fe81a608', builtLandingPage, console.log)
 }
 
