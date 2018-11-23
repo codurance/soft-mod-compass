772cf678af50032a2df45411f9ae4dc9407938d5
Dan Parkin <daniel.parkin@codurance.com>
2018-11-23 12:40:12 +0000

    chore: clean up parameter store testing files

--- scripts/docker/param-store-test.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# export HUBSPOT_AUTH_TOKEN=$(aws ssm get-parameter --name "/Prod/Compass/hubspot-auth-token" --with-decryption --query Parameter.Value --output text)
-echo $HUBSPOT_AUTH_TOKEN
--- src/hubspot/index.js
+++ src/hubspot/index.js
@@ -4,8 +4,6 @@ const readFile = require('./readFile')
 
 const startDeployment = async () => {
   const builtLandingPage = build(readFile)
-  console.log(process.env.HUBSPOT_AUTH_TOKEN, 'hubspot')
-  console.log(process.env, 'env vars')
   const authToken = process.env.HUBSPOT_AUTH_TOKEN
   await deploy(authToken, builtLandingPage, console.log)
 }
