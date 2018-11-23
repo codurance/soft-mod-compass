2154d5f613e7fc1dcdad90667e51fa4d399bfda5
Dan Parkin <daniel.parkin@codurance.com>
2018-11-23 11:45:33 +0000

    chore: test getting hubspot token from parameter store

--- scripts/docker/param-store-test.sh
+++ scripts/docker/param-store-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export HUBSPOT_TOKEN=$(aws ssm get-parameter --name "HubSpot" --with-decryption --query Parameter.Value)
-echo $HUBSPOT_TOKEN
+export HUBSPOT_AUTH_TOKEN=$(aws ssm get-parameter --name "/Prod/Compass/hubspot-auth-token" --with-decryption --query Parameter.Value --output text)
+echo $HUBSPOT_AUTH_TOKEN
--- src/hubspot/index.js
+++ src/hubspot/index.js
@@ -6,7 +6,8 @@ const startDeployment = async () => {
   const builtLandingPage = build(readFile)
   console.log(process.env.HUBSPOT_AUTH_TOKEN, 'hubspot')
   console.log(process.env, 'env vars')
-  await deploy('256e3ebf-f076-4016-a6c9-c787fe81a608', builtLandingPage, console.log)
+  const authToken = process.env.HUBSPOT_AUTH_TOKEN
+  await deploy(authToken, builtLandingPage, console.log)
 }
 
 startDeployment()
