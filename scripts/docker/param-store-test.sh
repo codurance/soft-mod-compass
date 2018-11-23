363972dd135b73966d09656e0d1a2312f388d545
Dan Parkin <daniel.parkin@codurance.com>
2018-11-23 12:18:03 +0000

    chore: try using parameter-store in buildspec to retrieve hubspot token

--- scripts/docker/param-store-test.sh
+++ scripts/docker/param-store-test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export HUBSPOT_AUTH_TOKEN=$(aws ssm get-parameter --name "/Prod/Compass/hubspot-auth-token" --with-decryption --query Parameter.Value --output text)
+# export HUBSPOT_AUTH_TOKEN=$(aws ssm get-parameter --name "/Prod/Compass/hubspot-auth-token" --with-decryption --query Parameter.Value --output text)
 echo $HUBSPOT_AUTH_TOKEN
