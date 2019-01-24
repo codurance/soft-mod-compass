40077a1e4a957472dd52c9a3bc70fadbdbadb712
Scott Edwards <scotteza@gmail.com>
2019-01-24 13:23:03 +0000

    Fix bug with dash-to-underscore replacement

--- src/server/report/contentRepository.js
+++ src/server/report/contentRepository.js
@@ -12,7 +12,7 @@ const loadContentFor = (categoryName, score) => {
 }
 
 function fileNameFor(categoryName, level) {
-    let safeCategoryName = categoryName.toLowerCase().replace(/ /g,"_")
+    let safeCategoryName = categoryName.toLowerCase().replace(/ /g,"_").replace("-", "_")
     return `${__dirname}/../../../data/content/${safeCategoryName}_${level}.html`
 }
 
