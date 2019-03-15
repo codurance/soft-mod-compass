d8dcf2aeb044dcf4cfdf6df5fcb44bb8d9327c46
Scott Edwards <scotteza@gmail.com>
2019-03-15 11:42:43 +0000

    Refactor static content repository and remove unneeded console log statement

--- src/server/report/staticContentRepository.js
+++ src/server/report/staticContentRepository.js
@@ -3,8 +3,7 @@ const fs = require('fs')
 const loadContentFor = (categoryName) => {
   try {
     const content = fs.readFileSync(fileNameFor(categoryName), 'UTF-8')
-    const contentWithNoLineBreaks = '<p>' + content.replace(/\n/g, ' ').replace(/\<p\>/g, '').replace(/\<\/p\>/g, '') + '</p>'
-    console.log(contentWithNoLineBreaks)
+    const contentWithNoLineBreaks = '<p>' + content.replace(/\n/g, ' ').replace(/<p>/g, '').replace(/<\/p>/g, '') + '</p>'
     return {
       content,
       contentWithNoLineBreaks
