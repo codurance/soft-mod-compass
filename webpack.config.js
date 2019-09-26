eaf688d043231ef0ed01ea7b9702936aebd583d6
Daniel <daniel.bird@codurance.com>
2019-09-26 12:41:37 +0100

    Add Clean Webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,14 @@
-const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   entry: './src/client/index.js',
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
-}
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
