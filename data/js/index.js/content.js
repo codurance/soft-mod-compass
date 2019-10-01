03c845a0d587e6cdff4a1b7635ed18e46be0160b
Daniel <daniel.bird@codurance.com>
2019-10-01 11:58:02 +0100

    Added functionality to update graphs on the Assessment Page

--- data/js/index.js/content.js
+++ data/js/index.js/content.js
@@ -1,4 +1,4 @@
-(function insertCopyright() {
+(function insertCopyright(data) {
   const PAGES = document.querySelectorAll('[id$="-page"]');
   const COPYRIGHT_HTML = `<div class="copyright">&copy; Codurance ${new Date().getFullYear()}</div>`;
 
@@ -6,3 +6,60 @@
     page.innerHTML += COPYRIGHT_HTML;
   }
 })();
+
+
+
+
+
+const updateAssessmentBars = scores => {
+  const GRAPHS = [...document.querySelectorAll('.assessment-page_graph .graph-bar_result-bar')];
+
+  for (let graph of GRAPHS) {
+    graph.style.width = `${scores[GRAPHS.indexOf(graph)]}%`;
+  }
+}
+
+const updateAssessmentText = scores => {
+  const TEXTS = [...document.querySelectorAll('.assessment-page_result-text')];
+    
+  for (let text of TEXTS) {
+      let alignmentText = scores[TEXTS.indexOf(text)] < 33.3
+                          ? 'Somewhat Aligned'
+                          : scores[TEXTS.indexOf(text)] < 66.6
+                          ? 'Almsot Aligned'
+                          : 'Closely Aligned';
+                          
+    text.innerHTML = alignmentText;
+  }
+}
+
+(function updateAssessmentGraphs(scores) {
+  updateAssessmentBars(scores);
+  updateAssessmentText(scores);
+})([{{summaryRadial.scores}}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
