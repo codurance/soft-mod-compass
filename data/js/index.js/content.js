e04565ad46c58d9dcefbedc344a470b14070cdaf
Daniel <daniel.bird@codurance.com>
2019-10-09 09:50:45 +0100

    Added function to add current date to front page of report

--- data/js/index.js/content.js
+++ data/js/index.js/content.js
@@ -8,6 +8,17 @@
 })();
 
 
+(function insertDate() {
+    const dateSection = document.querySelector('.front-page_date-name-vars');
+    
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0');
+    var yyyy = today.getFullYear();
+
+    dateSection.innerHTML = `${dd}/${mm}/${yyyy}`;
+})();
+
 
 
 
