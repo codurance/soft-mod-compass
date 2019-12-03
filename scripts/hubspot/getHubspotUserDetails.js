dd4354b889d51d97f5362ed716b411ead80e563f
Daniel <info@daniel-bird.co.uk>
2019-12-03 10:51:38 +0000

    Added Hubspot Env entries to server config
    Added the Hubspot authorisation and form ID to the server config file, so that AWS can pick the environment variables up when running Docker.

--- scripts/hubspot/getHubspotUserDetails.js
+++ scripts/hubspot/getHubspotUserDetails.js
@@ -1,8 +1,9 @@
 const rp = require('request-promise')
+const config = require('../../src/server/config')
 
 function getHubspotUserDetails (uuid) {
-  const authorisation = process.env.HUBSPOT_AUTH_TOKEN
-  const formId = process.env.HUBSPOT_FORM_ID
+  const authorisation = config.hubspot.authToken
+  const formId = config.hubspot.formId
   const endpoint = `https://api.hubapi.com/form-integrations/v1/submissions/forms/${formId}?hapikey=${authorisation}`
 
   if (typeof (uuid) !== 'string') console.error('uuid is not a string')
--- src/server/config.js
+++ src/server/config.js
@@ -8,5 +8,9 @@ module.exports = {
     url: process.env.TYPEFORM_URL,
     formId: process.env.TYPEFORM_FORM_ID,
     authToken: process.env.TYPEFORM_AUTH_TOKEN
+  },
+  hubspot: {
+    formId: process.env.HUBSPOT_FORM_ID,
+    authToken: process.env.HUBSPOT_AUTH_TOKEN
   }
 }
