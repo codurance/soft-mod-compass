3c1e8b802a04425d7b1a1a1343c2d270f8da6ce7
Daniel <info@daniel-bird.co.uk>
2019-12-06 15:22:22 +0000

    Added request-promise debugging module for debugging once deployed

--- scripts/hubspot/getHubspotUserDetails.js
+++ scripts/hubspot/getHubspotUserDetails.js
@@ -1,21 +1,44 @@
 const rp = require('request-promise')
+require('request-promise').debug = true
+require('request-debug')(rp)
 const config = require('../../src/server/config')
 
 function getHubspotUserDetails (uuid) {
-  const authorisation = config.hubspot.authToken
   const formId = config.hubspot.formId
-  const endpoint = `https://api.hubapi.com/form-integrations/v1/submissions/forms/${formId}?hapikey=${authorisation}`
+  const endpoint = `https://api.hubapi.com/form-integrations/v1/submissions/forms/${formId}`
 
   if (typeof (uuid) !== 'string') console.error('uuid is not a string')
 
   const options = {
     uri: endpoint,
+    qs: {
+      hapikey: config.hubspot.authToken
+    },
     json: true
   }
 
+  // return rp(options, (error, response, body) => {
+  //   console.error('error:', error)
+  //   console.log('status code:', respone && response.statusCode)
+  //   console.log('body:', body)
+
+  // }).then(response => {
+  //   // if(request.readyState === XMLHttpRequest.DONE) console.log('REQUEST FOR HUBSPOT DONE')
+
+  //   console.log('RETREIVED HUBSPOT USER DATA')
+  //   const data = response.results.filter(result => result.values[4].value === uuid)
+  //   console.log('FILTERED HUBSPOT USER DATA, RETURNING DATA NOW')
+  //   return data[0]
+  // })
+  //   .catch(err => console.error(err))
+
   return rp(options).then(response => {
+    // if(request.readyState === XMLHttpRequest.DONE) console.log('REQUEST FOR HUBSPOT DONE')
+
     console.log('RETREIVED HUBSPOT USER DATA')
     const data = response.results.filter(result => result.values[4].value === uuid)
+    console.dir(data)
+
     console.log('FILTERED HUBSPOT USER DATA, RETURNING DATA NOW')
     return data[0]
   })
--- src/server/report/reportViewModel.js
+++ src/server/report/reportViewModel.js
@@ -8,7 +8,7 @@ function reportViewModel (categories, questionChoices, answers, userDetails) {
   const userData = userDetails
 
   console.log(`ABOUT TO MAKE VIEW MODAL JSON RESULTS:
-    userData: ${userData}`)
+    userData: ${JSON.stringify(userData)}`)
 
   const jsonResults = {
     userData,
