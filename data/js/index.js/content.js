82afaf09802c9eeb7b63061d429b461dbd6e24a3
Daniel <info@daniel-bird.co.uk>
2019-12-10 11:08:43 +0000

    Adjusted the main HTML template to render correct breakdown pages.
    Instead of using javascript to render the correct breakdown page background, I'm utilising Handlebars to render the correct background image according to the users scores.

--- data/js/index.js/content.js
+++ data/js/index.js/content.js
@@ -61,38 +61,18 @@ const updateAssessmentText = scores => {
                   document.querySelectorAll('#cft-results-page .graph-bar_result-bar'),
                   document.querySelectorAll('#xpp-results-page .graph-bar_result-bar')
                  ];
-  
+
   let data = [];
-        
+
   {{#each categories}}
     data.push([{{this.subCategoryScores}}]);
   {{/each}}
-  
+
  for (let i = 0; i < GRAPHS.length; i++) {
-      
+
       for (let j = 0; j < GRAPHS[i].length; j++) {
           GRAPHS[i][j].style.width = `${data[i][j]}%`;
       }
-      
+
   }
 })();
-
-
-
-
-
-(function insertBreakdownPages(scores) {
-  const BREAKDOWN_PAGES = [...document.querySelectorAll('[id$="-breakdown-page"]')];
-
-  for (let breakdownPage of BREAKDOWN_PAGES) {
-    let result = scores[BREAKDOWN_PAGES.indexOf(breakdownPage)] < 33
-                 ? 'bad'
-                 : scores[BREAKDOWN_PAGES.indexOf(breakdownPage)] < 66
-                 ? 'avg'
-                 : 'good';
-                 
-    breakdownPage.style.background       = `url(assets/pageImgs/${breakdownPage.id}-${result}.jpg)`;
-    breakdownPage.style.backgroundRepeat = 'no-repeat';
-    breakdownPage.style.backgroundSize   = 'cover';
-  }
-})([{{summaryRadial.scores}}]);
