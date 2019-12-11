af10ca25f1e1cc87661c9f2d88fbbbefd86cdb3a
Daniel <info@daniel-bird.co.uk>
2019-12-11 10:11:53 +0000

    Add retries to the getHubspotUserDetails request if results are empty.

--- scripts/hubspot/getHubspotUserDetails.js
+++ scripts/hubspot/getHubspotUserDetails.js
@@ -1,16 +1,14 @@
 const rp = require('request-promise')
+const sleep = require('sleep-promise')
 // require('request-promise').debug = true
 // require('request-debug')(rp)
 const config = require('../../src/server/config')
 
-function getHubspotUserDetails (uuid) {
-  const formId = config.hubspot.formId
-  const endpoint = `https://api.hubapi.com/form-integrations/v1/submissions/forms/${formId}`
-
+function getHubspotUserDetails (uuid, retries = 3) {
   if (typeof (uuid) !== 'string') console.error('uuid is not a string')
 
   const options = {
-    uri: endpoint,
+    uri: `https://api.hubapi.com/form-integrations/v1/submissions/forms/${config.hubspot.formId}`,
     qs: {
       hapikey: config.hubspot.authToken
     },
@@ -18,12 +16,17 @@ function getHubspotUserDetails (uuid) {
   }
 
   return rp(options).then(response => {
-    console.log('RETREIVED HUBSPOT USER DATA')
-    const data = response.results.filter(result => result.values[4].value === uuid)
-    console.dir(data)
-
-    console.log('FILTERED HUBSPOT USER DATA, RETURNING DATA NOW')
-    return data[0]
+    if (response.results.length > 0) {
+      console.log('RETREIVED HUBSPOT USER DATA')
+      const data = response.results.filter(result => result.values[4].value === uuid)
+      console.dir(data)
+      console.log('FILTERED HUBSPOT USER DATA, RETURNING DATA NOW')
+      return data[0]
+    } else {
+      const retriesLeft = retries - 1
+      if (retriesLeft === 0) throw Error(`no data retreived for ${uuid} after three attempts`)
+      return sleep(250).then(() => getHubspotUserDetails(uuid, retriesLeft))
+    }
   })
     .catch(err => console.error(err))
 }
