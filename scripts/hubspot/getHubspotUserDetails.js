b2695f857e18c12cf8d14bc0dc94e192fe136754
Daniel <info@daniel-bird.co.uk>
2019-12-13 12:16:45 +0000

    [Feature] Capitalises outputs name and company in title case

--- scripts/hubspot/getHubspotUserDetails.js
+++ scripts/hubspot/getHubspotUserDetails.js
@@ -13,17 +13,37 @@ function getHubspotUserDetails (uuid, retries = 3) {
     json: true
   }
 
-  return rp(options).then(response => {
-    if (response.results.length > 0) {
-      const data = response.results.filter(result => result.values[4].value === uuid)
-      return data[0]
-    } else {
-      const retriesLeft = retries - 1
-      if (retriesLeft === 0) throw Error(`no data retreived for ${uuid} after three attempts`)
-      return sleep(250).then(() => getHubspotUserDetails(uuid, retriesLeft))
+  function titleCase (str) {
+    const names = str.toLowerCase().split(' ')
+    for (let i = 0; i < names.length; i++) {
+      names[i] = names[i][0].toUpperCase() + names[i].slice(1)
     }
+    return names.join(' ')
+  }
+
+  return sleep(2000).then(() => {
+    return rp(options).then(response => {
+      if (response.results.length > 0) {
+        const data = response.results.filter(result => result.values[4].value === uuid)[0]
+
+        data.values = data.values.map(entry => {
+          if (entry.name === 'firstname' || entry.name === 'lastname' || entry.name === 'company') {
+            entry.value = titleCase(entry.value)
+            return entry
+          } else {
+            return entry
+          }
+        })
+
+        return data
+      } else {
+        const retriesLeft = retries - 1
+        if (retriesLeft === 0) throw Error(`no data retreived for ${uuid} after three attempts`)
+        return sleep(350).then(() => getHubspotUserDetails(uuid, retriesLeft))
+      }
+    })
+      .catch(err => console.error(err))
   })
-    .catch(err => console.error(err))
 }
 
 module.exports = getHubspotUserDetails
--- src/server/report/reportViewModelBuilder.js
+++ src/server/report/reportViewModelBuilder.js
@@ -2,12 +2,14 @@ const reportViewModel = require('./reportViewModel')
 const categoryData = require('./categoryData')
 
 const reportViewModelBuilder = (typeformClient, getHubspotUserDetails) => {
-
   async function buildReportViewModelFor (submissionId) {
     try {
       const choices = await typeformClient.getQuestionChoices()
       const answers = await typeformClient.surveyAnswersFor(submissionId)
       const userDetails = await getHubspotUserDetails(submissionId)
+
+      console.log('USER DETES:', userDetails)
+
       return reportViewModel(categoryData, choices, answers, userDetails)
     } catch (error) {
       console.log(error)
