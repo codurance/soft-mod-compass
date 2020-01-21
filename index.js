9e172c4e704009e78f9f83c611d76d7a251e04b2
David Welch <david.welch@codurance.com>
2020-01-21 11:45:27 +0000

    formatting

--- index.js
+++ index.js
@@ -59,14 +59,14 @@ jsreport
     console.error(e)
   })
 
-process.on('SIGTERM', function() {
+process.on('SIGTERM', function () {
   server.close(() => {
     console.log('Received SIGTERM, shutting down')
     process.exit(0)
   })
 })
 
-process.on('SIGINT', function() {
+process.on('SIGINT', function () {
   console.log('Received SIGINT, shutting down')
   server.close(() => {
     process.exit(0)
