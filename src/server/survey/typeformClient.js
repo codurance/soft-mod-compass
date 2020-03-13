e5aeffb0fe3fdeca42fdd1adc9d31b2335b1ab61
aclaudel <arnaud.claudel@codurance.com>
2020-03-13 13:55:20 +0000

    increase the timeout delay to 1s and the retries to 15

--- src/server/survey/typeformClient.js
+++ src/server/survey/typeformClient.js
@@ -11,7 +11,7 @@ module.exports = config => {
     getQuestionChoices
   }
 
-  function surveyAnswersFor (uuid, retries = 3) {
+  function surveyAnswersFor (uuid, retries = 15) {
     const options = {
       uri: `${config.typeform.url}/forms/${config.typeform.formId}/responses?query=${uuid}`,
       headers,
@@ -27,7 +27,7 @@ module.exports = config => {
           throw Error(`no survey answers for ${uuid} after three attempts`)
         }
 
-        return sleep(250).then(() => surveyAnswersFor(uuid, retriesLeft))
+        return sleep(1000).then(() => surveyAnswersFor(uuid, retriesLeft))
       }
     })
   }
