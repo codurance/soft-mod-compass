52b2701f31c004f75b1d8fe1825f7664ce3a6111
aclaudel <arnaud.claudel@codurance.com>
2020-03-19 14:40:03 +0000

    make s3 error message more meaningful

--- src/server/aws/uploadPdfToS3AndSendEmail.js
+++ src/server/aws/uploadPdfToS3AndSendEmail.js
@@ -9,7 +9,7 @@ function uploadPdfToS3AndSendEmail (email, pdf) {
       console.log(`pdf available at ${pdfLink}`)
       sendPdfLinkEmail(email, pdfLink)
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log(`an error occurred while uploading the pdf\n${err}`))
 }
 
 module.exports = uploadPdfToS3AndSendEmail
