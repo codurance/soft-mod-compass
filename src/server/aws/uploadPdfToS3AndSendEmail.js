78e271b1a0261a9e501f735855ebc70f194d6e3f
aclaudel <arnaud.claudel@codurance.com>
2020-03-19 10:57:27 +0000

    rearrange code

--- src/server/aws/uploadPdfToS3AndSendEmail.js
+++ src/server/aws/uploadPdfToS3AndSendEmail.js
@@ -1,31 +1,37 @@
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
 const stream = require('stream')
-const sendEmail = require('../mail/sendEmail')
+const sendEmail = require('./sendEmail')
 
-function uploadPdfToS3AndSendEmail (viewModel, out) {
-  var pass = new stream.PassThrough()
+function uploadPdfToS3AndSendEmail (viewModel, pdf) {
+  const pdfStreamPipe = new stream.PassThrough()
 
-  // Setting up S3 upload parameters
   const params = {
+    // TODO extract bucket name as env variable
     Bucket: 'compass-pdf',
-    Key: 'test.pdf', // File name you want to save as in S3
-    Body: pass,
+    // TODO generate a proper name
+    Key: 'test.pdf',
+    Body: pdfStreamPipe,
     ACL: 'public-read'
   }
 
-  // Uploading files to the bucket
   s3.upload(params).promise()
-      .then((data) => {
-        const email = viewModel.userData.values.find(d => d.name === 'email').value
-        const pdfLink = data.Location
-
-        console.log(`pdf available at ${pdfLink}`)
-        sendEmail(email, pdfLink)
-      })
+      .then(sendPdfLinkEmail)
       .catch(err => console.log(err))
 
-  out.stream.pipe(pass)
+  function sendPdfLinkEmail (data) {
+    const email = getEmail(viewModel)
+    const pdfLink = data.Location
+
+    console.log(`pdf available at ${pdfLink}`)
+    sendEmail(email, pdfLink)
+  }
+
+  function getEmail (viewModel) {
+    return viewModel.userData.values.find(d => d.name === 'email').value
+  }
+
+  pdf.stream.pipe(pdfStreamPipe)
 }
 
 module.exports = uploadPdfToS3AndSendEmail
