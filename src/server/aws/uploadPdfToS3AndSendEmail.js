bdb9ebf060bd19859136c8440a3a056305d703f1
aclaudel <arnaud.claudel@codurance.com>
2020-03-19 13:55:32 +0000

    rename pdf link promise variable

--- src/server/aws/uploadPdfToS3AndSendEmail.js
+++ src/server/aws/uploadPdfToS3AndSendEmail.js
@@ -2,9 +2,9 @@ const sendPdfLinkEmail = require('./sendPdfLinkEmail')
 const uploadToS3 = require('./uploadToS3')
 
 function uploadPdfToS3AndSendEmail (email, pdf) {
-  const s3Promise = uploadToS3(pdf)
+  const pdfLinkPromise = uploadToS3(pdf)
 
-  s3Promise
+  pdfLinkPromise
     .then(pdfLink => {
       console.log(`pdf available at ${pdfLink}`)
       sendPdfLinkEmail(email, pdfLink)
