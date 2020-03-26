36f40e75015c90a042ce67f91554e50f9be7ee0f
aclaudel <arnaud.claudel@codurance.com>
2020-03-26 22:20:38 +0000

    remove unused variable

--- scripts/aws/deploy.sh
+++ scripts/aws/deploy.sh
@@ -1,32 +1,23 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname $0)
-
 APP_NAME='compass'
 ENV_NAME='integration'
 FULL_NAME=${APP_NAME}-${ENV_NAME}
-ROLE="role-"${FULL_NAME}
-POLICY="policy-${FULL_NAME}"
-export INSTANCE_PROFILE="instance-profile-${FULL_NAME}"
+# exported because it's used in option-settings-update.json
 export BUCKET="bucket-${FULL_NAME}"
 VERSION_LABEL=$(uuid)
-STACK_NAME='64bit Amazon Linux 2018.03 v2.14.2 running Docker 18.09.9-ce'
 ARTIFACT='aws-artifact.zip'
 ARTIFACT_S3="s3://${BUCKET}/${ARTIFACT}"
-TRUST_FILE="file://${BASEDIR}/iam/compass-trust.json"
 POLICY_FILE="${BASEDIR}/iam/compass-policies.json"
 OPTION_SETTINGS_FILE="${BASEDIR}/eb/option-settings.json"
 OPTION_SETTINGS_FILE_FOR_UPDATE="${BASEDIR}/eb/option-settings-update.json"
-EB_FULL_ACCESS="arn:aws:iam::aws:policy/AWSElasticBeanstalkFullAccess"
-S3_LIFECYCLE_FILE="file://${BASEDIR}/s3/s3-lifecycle.json"
 
 loadFileAndReplaceEnvVariables() {
     envsubst < "${1}"
 }
 
 OPTION_SETTINGS_FOR_UPDATE=$(loadFileAndReplaceEnvVariables "${OPTION_SETTINGS_FILE_FOR_UPDATE}")
-OPTION_SETTINGS=$(loadFileAndReplaceEnvVariables "${OPTION_SETTINGS_FILE}")
-POLICY_DOCUMENT=$(loadFileAndReplaceEnvVariables "${POLICY_FILE}")
 
 git archive master -o ${ARTIFACT}
 
