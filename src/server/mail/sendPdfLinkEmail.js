5a06cff40ec471566b73d17052fdf959ae59008e
Daniel <info@daniel-bird.co.uk>
2020-03-31 13:54:17 +0100

    Update from address for successful submission email

--- src/server/mail/sendPdfLinkEmail.js
+++ src/server/mail/sendPdfLinkEmail.js
@@ -1,24 +1,30 @@
-const AWS = require('aws-sdk')
-const ses = new AWS.SES({ region: 'eu-west-1' })
+const AWS = require('aws-sdk');
+const ses = new AWS.SES({ region: 'eu-west-1' });
 const pdfLinkEmail = require('./PdfLinkEmail');
 
-function sendPdfLinkEmail (pdfLink, userData) {
+function sendPdfLinkEmail(pdfLink, userData) {
   const emailData = {
-    Source: 'compass@codurance.com',
+    Source: 'talkwithus@codurance.com',
     Destination: {
-      ToAddresses: [userData.email]
+      ToAddresses: [userData.email],
     },
     Message: {
       Subject: { Data: 'Here is your Codurance Compass report' },
       Body: {
-        Html: { Data: pdfLinkEmail(pdfLink, userData) }
-      }
-    }
-  }
+        Html: { Data: pdfLinkEmail(pdfLink, userData) },
+      },
+    },
+  };
 
-  ses.sendEmail(emailData).promise()
+  ses
+    .sendEmail(emailData)
+    .promise()
     .then(_ => console.log(`pdf sent to [${userData.email}]`))
-    .catch(err => console.log(`an error occurred while sending pdf to [${userData.email}]\n${err}`))
+    .catch(err =>
+      console.log(
+        `an error occurred while sending pdf to [${userData.email}]\n${err}`
+      )
+    );
 }
 
-module.exports = sendPdfLinkEmail
+module.exports = sendPdfLinkEmail;
