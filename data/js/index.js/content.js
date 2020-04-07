1fd2edd8d024c9a43fd638787dafaad02b515fe3
Daniel <info@daniel-bird.co.uk>
2020-04-07 17:44:41 +0100

    Fine tune Scoring Thresholds
    Also include a new icon for an above average score.

--- data/js/index.js/content.js
+++ data/js/index.js/content.js
@@ -40,9 +40,9 @@ function updateAssessmentBars(scores) {
     
     graph.style.width = `${currentScore}%`;
     
-    if (currentScore > 80) {
+    if (currentScore > 75) {
         graph.classList.add('good-result');
-    } else if (currentScore >= 60) {
+    } else if (currentScore >= 50) {
         graph.classList.add('average-result');
     } else {
         graph.classList.add('bad-result');
@@ -77,12 +77,15 @@ function updateCategoryGraphs(subCategoryScores) {
             
             innerFill.style.width = `${score}%`;
             
-                if (score )
-                if (score > 80) {
+                if (score === 100) {
                     innerFill.classList.add('good-result');
                     rule.classList.add('good-result');
                     graph.querySelector('.main-bar-graph__good-icon').classList.remove('hidden');
-                } else if (score >= 60) {
+                }else if (score === 80) {
+                    innerFill.classList.add('good-result');
+                    rule.classList.add('good-result');
+                    graph.querySelector('.main-bar-graph__average-high-icon').classList.remove('hidden');
+                } else if (score === 60) {
                     innerFill.classList.add('average-result');
                     rule.classList.add('average-result');
                     graph.querySelector('.main-bar-graph__average-icon').classList.remove('hidden');
@@ -107,11 +110,11 @@ function updateSummaryBars(scores) {
         
         innerFill.style.width = `${currentScore}%`;
         
-        if (currentScore > 80) {
+        if (currentScore > 75) {
           innerFill.classList.add('good-result');
           rule.classList.add('good-result');
           container.querySelector('.summary-bar-graph__good-icon').classList.remove('hidden');
-        } else if (currentScore >= 60) {
+        } else if (currentScore >= 50) {
           innerFill.classList.add('average-result');
           rule.classList.add('average-result');
           container.querySelector('.summary-bar-graph__average-icon').classList.remove('hidden');
