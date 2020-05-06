68de81642f40fd6283108b78f949331ddcf4d4cf
Florian Kempenich <Flori@nKempenich.com>
2020-05-06 13:54:05 +0100

    Refactor deploy script

--- scripts/aws/deploy.sh
+++ scripts/aws/deploy.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
+function stop_on_first_failure { set -e -o pipefail; }
 
-if [[ $# -lt 1 ]] ; then
-    echo "ERROR: missing target environment"
-    exit 1
-fi
-
-BASEDIR=$(dirname $0)
-ENV_NAME=$1
-APP_NAME='compass'
-FULL_NAME=${APP_NAME}-${ENV_NAME}
-VERSION_LABEL=$(uuidgen)
-ARTIFACT='aws-artifact.zip'
-POLICY_FILE="${BASEDIR}/iam/compass-policies.json"
-OPTION_SETTINGS_FILE="${BASEDIR}/eb/option-settings.json"
-OPTION_SETTINGS_FILE_FOR_UPDATE="${BASEDIR}/eb/option-settings-update.json"
-
-loadFileAndReplaceEnvVariables() {
-    envsubst < "${1}"
+function ensure_env_name_provided_as_argument {
+    if [[ $# -lt 1 ]] ; then
+        echo "ERROR: missing target environment"
+        exit 1
+    fi
 }
 
-OPTION_SETTINGS_FOR_UPDATE=$(loadFileAndReplaceEnvVariables "${OPTION_SETTINGS_FILE_FOR_UPDATE}")
+function initialize_variables {
+    ensure_env_name_provided_as_argument $1
+    ENV_NAME=$1
+    . ${BASEDIR}/variables.sh $ENV_NAME
+    VERSION_LABEL=$(uuidgen)
+    ARTIFACT='aws-artifact.zip'
+}
 
-echo "building and uploading artifact [${ARTIFACT}] .."
-git archive master -o ${ARTIFACT}
+function build_artifact_for_master {
+    echo "Building and uploading artifact [${ARTIFACT}] .."
+    git archive master -o ${ARTIFACT}
+}
 
-BUCKET=$(aws elasticbeanstalk create-storage-location | jq -r '.S3Bucket')
-ARTIFACT_S3="s3://${BUCKET}/${VERSION_LABEL}-${ARTIFACT}"
+function upload_artifact_to_s3 {
+    ARTIFACT_BUCKET=$(aws elasticbeanstalk create-storage-location | jq -r '.S3Bucket')
+    ARTIFACT_S3="s3://${ARTIFACT_BUCKET}/${VERSION_LABEL}-${ARTIFACT}"
+    aws s3 cp ${ARTIFACT} ${ARTIFACT_S3}
+}
 
-aws s3 cp ${ARTIFACT} ${ARTIFACT_S3}
+function deploy_artifact_to_elasticbean_and_set_envvars {
+    echo "creating application version  [${VERSION_LABEL}] .."
+    aws elasticbeanstalk create-application-version \
+        --application-name ${APP_NAME} \
+        --version-label ${VERSION_LABEL} \
+        --source-bundle "S3Bucket=${ARTIFACT_BUCKET},S3Key=${VERSION_LABEL}-${ARTIFACT}"
 
-echo "creating application version  [${VERSION_LABEL}] .."
-aws elasticbeanstalk create-application-version \
-    --application-name ${APP_NAME} \
-    --version-label ${VERSION_LABEL} \
-    --source-bundle "S3Bucket=${BUCKET},S3Key=${VERSION_LABEL}-${ARTIFACT}"
+    echo "updating environment  [${ENV_NAME}] .."
+    aws elasticbeanstalk update-environment \
+        --environment-name ${ENV_NAME} \
+        --version-label ${VERSION_LABEL} \
+        --option-settings "${OPTION_SETTINGS_FOR_UPDATE}"
+}
 
-echo "updating environment  [${ENV_NAME}] .."
-aws elasticbeanstalk update-environment \
-    --environment-name ${ENV_NAME} \
-    --version-label ${VERSION_LABEL} \
-    --option-settings "${OPTION_SETTINGS_FOR_UPDATE}"
+stop_on_first_failure
+initialize_variables $1
+build_artifact_for_master
+upload_artifact_to_s3
+deploy_artifact_to_elasticbean_and_set_envvars
