6478cb219eb50aad1df07fcabc85cab6089bd400
Florian Kempenich <Flori@nKempenich.com>
2020-05-06 13:08:30 +0100

    Refactor env creation & Add env destruction script
    Co-authored-by: cherryt <cherr_y_t@hotmail.com>

---
+++ scripts/aws/destroy-env.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"  # src: https://stackoverflow.com/a/246128/4490991
+
+function ensure_env_name_provided_as_argument {
+    if [[ $# -lt 1 ]] ; then
+        echo "ERROR: missing target environment"
+        exit 1
+    fi
+}
+
+function initialize_variables {
+    ensure_env_name_provided_as_argument $1
+    ENV_NAME=$1
+    . ${BASEDIR}/variables.sh $ENV_NAME
+}
+
+function terminate_environment {
+    echo "Terminating Environmnent [${ENV_NAME}] ..."
+    aws elasticbeanstalk terminate-environment \
+        --environment-name ${ENV_NAME}
+}
+
+function delete_instance_profile {
+    echo "Removing Role [${ROLE}] from Instance Profile [${INSTANCE_PROFILE}] ..."
+    aws iam remove-role-from-instance-profile \
+        --instance-profile-name ${INSTANCE_PROFILE} \
+        --role-name ${ROLE}
+        
+    echo "Deleting Instance Profile [${INSTANCE_PROFILE}] ..."
+    aws iam delete-instance-profile \
+        --instance-profile-name ${INSTANCE_PROFILE}
+}
+
+function delete_compass_role {
+    echo "Deleting Role [${ROLE}] ..."
+    aws iam delete-role-policy \
+        --role-name ${ROLE} \
+        --policy-name ${POLICY}
+
+    aws iam delete-role \
+        --role-name ${ROLE}
+}
+
+function delete_s3_bucket {
+    echo "Deleting compass bucket [${BUCKET}] ..."
+    aws s3 rb s3://${BUCKET} \
+        --force
+}
+
+
+initialize_variables $1
+terminate_environment
+delete_instance_profile
+delete_compass_role
+delete_s3_bucket
---
+++ scripts/aws/variables.sh
@@ -0,0 +1,25 @@
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # src: https://stackoverflow.com/a/246128/4490991
+
+ENV_NAME=$1
+APP_NAME='compass'
+FULL_NAME=${APP_NAME}-${ENV_NAME}
+ROLE="role-"${FULL_NAME}
+POLICY="policy-${FULL_NAME}"
+# exported because it's used in option-settings.json
+export INSTANCE_PROFILE="instance-profile-${FULL_NAME}"
+# exported because it's used in compass-policies.json
+export BUCKET="bucket-${FULL_NAME}"
+STACK_NAME='64bit Amazon Linux 2018.03 v2.15.0 running Docker 19.03.6-ce'
+TRUST_FILE="file://${BASEDIR}/iam/compass-trust.json"
+POLICY_FILE="${BASEDIR}/iam/compass-policies.json"
+OPTION_SETTINGS_FILE="${BASEDIR}/eb/option-settings.json"
+OPTION_SETTINGS_FILE_FOR_UPDATE="${BASEDIR}/eb/option-settings-update.json"
+EB_FULL_ACCESS="arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
+S3_LIFECYCLE_FILE="file://${BASEDIR}/s3/s3-lifecycle.json"
+
+loadFileAndReplaceEnvVariables() {
+    envsubst < "${1}"
+}
+
+OPTION_SETTINGS=$(loadFileAndReplaceEnvVariables "${OPTION_SETTINGS_FILE}")
+POLICY_DOCUMENT=$(loadFileAndReplaceEnvVariables "${POLICY_FILE}")
