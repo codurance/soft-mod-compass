0bcfbf1749403349383be8a0f7a70c7fccb8584b
Florian Kempenich <Flori@nKempenich.com>
2020-05-06 13:08:30 +0100

    Refactor env creation & Add env destruction script
    Co-authored-by: cherryt <cherr_y_t@hotmail.com>

--- scripts/aws/create-env.sh
+++ scripts/aws/create-env.sh
@@ -1,70 +1,73 @@
 #!/bin/bash
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )" # src: https://stackoverflow.com/a/246128/4490991
 
-if [[ $# -lt 1 ]] ; then
-    echo "ERROR: missing target environment"
-    exit 1
-fi
+function stop_on_first_failure { set -e -o pipefail; }
 
-BASEDIR=$(dirname $0)
-ENV_NAME=$1
-APP_NAME='compass'
-FULL_NAME=${APP_NAME}-${ENV_NAME}
-ROLE="role-"${FULL_NAME}
-POLICY="policy-${FULL_NAME}"
-# exported because it's used in option-settings.json
-export INSTANCE_PROFILE="instance-profile-${FULL_NAME}"
-# exported because it's used in compass-policies.json
-export BUCKET="bucket-${FULL_NAME}"
-STACK_NAME='64bit Amazon Linux 2018.03 v2.14.3 running Docker 18.09.9-ce'
-TRUST_FILE="file://${BASEDIR}/iam/compass-trust.json"
-POLICY_FILE="${BASEDIR}/iam/compass-policies.json"
-OPTION_SETTINGS_FILE="${BASEDIR}/eb/option-settings.json"
-OPTION_SETTINGS_FILE_FOR_UPDATE="${BASEDIR}/eb/option-settings-update.json"
-EB_FULL_ACCESS="arn:aws:iam::aws:policy/service-role/AWSElasticBeanstalkService"
-S3_LIFECYCLE_FILE="file://${BASEDIR}/s3/s3-lifecycle.json"
-
-loadFileAndReplaceEnvVariables() {
-    envsubst < "${1}"
+function ensure_env_name_provided_as_argument {
+    if [[ $# -lt 1 ]] ; then
+        echo "ERROR: missing target environment"
+        exit 1
+    fi
 }
 
-OPTION_SETTINGS=$(loadFileAndReplaceEnvVariables "${OPTION_SETTINGS_FILE}")
-POLICY_DOCUMENT=$(loadFileAndReplaceEnvVariables "${POLICY_FILE}")
+function initialize_variables {
+    ensure_env_name_provided_as_argument $1
+    ENV_NAME=$1
+    . ${BASEDIR}/variables.sh $ENV_NAME
+}
 
-echo "creating compass bucket [${BUCKET}] .."
-aws s3 mb s3://${BUCKET}
+function create_and_configure_s3_bucket {
+    echo "creating compass bucket [${BUCKET}] .."
+    aws s3 mb s3://${BUCKET}
+    
+    aws s3api put-bucket-lifecycle-configuration \
+        --bucket ${BUCKET} \
+        --lifecycle-configuration "${S3_LIFECYCLE_FILE}"
+}
 
-aws s3api put-bucket-lifecycle-configuration \
-    --bucket ${BUCKET} \
-    --lifecycle-configuration "${S3_LIFECYCLE_FILE}"
+function create_role_to_allow_access_to_s3_and_send_mail {
+    echo "creating compass role: [${ROLE}] .."
+    aws iam create-role \
+        --role-name ${ROLE} \
+        --assume-role-policy-document "${TRUST_FILE}"
+    
+    aws iam put-role-policy \
+        --role-name ${ROLE} \
+        --policy-name ${POLICY} \
+        --policy-document "${POLICY_DOCUMENT}"
+}
 
-echo "creating compass role: [${ROLE}] .."
-aws iam create-role \
-    --role-name ${ROLE} \
-    --assume-role-policy-document "${TRUST_FILE}"
+function create_instance_profile_and_add_role_to_it {
+    echo "creating compass instance profile: [${INSTANCE_PROFILE}] .."
+    aws iam create-instance-profile \
+        --instance-profile-name ${INSTANCE_PROFILE}
+    
+    echo "adding role to instance profile .."
+    aws iam add-role-to-instance-profile \
+        --role-name ${ROLE} \
+        --instance-profile-name ${INSTANCE_PROFILE}
+}
 
-aws iam put-role-policy \
-    --role-name ${ROLE} \
-    --policy-name ${POLICY} \
-    --policy-document "${POLICY_DOCUMENT}"
+function create_env_with_instance_profile {
+    echo "creating compass environment [${ENV_NAME}] .."
+    aws elasticbeanstalk create-environment \
+        --application-name ${APP_NAME} \
+        --environment-name ${ENV_NAME} \
+        --solution-stack-name "${STACK_NAME}" \
+        --option-settings "${OPTION_SETTINGS}"
+}
 
-aws iam attach-role-policy \
-    --role-name ${ROLE} \
-    --policy-arn ${EB_FULL_ACCESS}
+function unnecessary_step_to_remove {
+    aws iam attach-role-policy \
+        --role-name ${ROLE} \
+        --policy-arn ${EB_FULL_ACCESS}
+}
 
-echo "creating compass instance profile: [${INSTANCE_PROFILE}] .."
-aws iam create-instance-profile \
-    --instance-profile-name ${INSTANCE_PROFILE}
 
-echo "adding role to instance profile .."
-aws iam add-role-to-instance-profile \
-    --role-name ${ROLE} \
-    --instance-profile-name ${INSTANCE_PROFILE}
-
-echo "creating compass environment [${ENV_NAME}] .."
-aws elasticbeanstalk create-environment \
-    --application-name ${APP_NAME} \
-    --environment-name ${ENV_NAME} \
-    --solution-stack-name "${STACK_NAME}" \
-    --option-settings "${OPTION_SETTINGS}"
-
-# TODO cleanup ?
+stop_on_first_failure
+initialize_variables $1
+create_and_configure_s3_bucket
+create_role_to_allow_access_to_s3_and_send_mail
+unnecessary_step_to_remove
+create_instance_profile_and_add_role_to_it
+create_env_with_instance_profile
