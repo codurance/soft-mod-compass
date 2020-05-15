2f662839525ace8b1347daa7b10cdf43a457c511
cherryt <cherry.tsui@codurance.com>
2020-05-15 13:33:50 +0100

    Generate spanish reports (#11)
    * spanish reports
    
    * Add Spanish page bgs to report
    
    * Fix page sizes
    
    * Fix front page text
    
    * Update Assessment page Image
    
    * Refactore ES styles into seperate file
    
    * Re-align Warning boxes on Assessment page
    
    * Refactor CSS
    
    * Update config time changes
    
    * Spanish template report updates
    
    * Update logic to use Spanish template for report
    
    * Use variable to insert copyright on ES & EN pages
    
    Co-authored-by: Daniel <info@daniel-bird.co.uk>

--- data/js/index.js/content.js
+++ data/js/index.js/content.js
@@ -11,7 +11,11 @@ window.onload = function() {
 }
 
 function insertCopyright() {
-  const PAGES = document.querySelectorAll('[id$="-page"]');
+  let pageId = "-page";
+  if(document.documentElement.lang === "es") {
+    pageId += "-es";
+  }
+  const PAGES = document.querySelectorAll(`[id$="${pageId}"]`);
   const COPYRIGHT_HTML = `<div class="copyright">&copy; Codurance ${new Date().getFullYear()}</div>`;
 
   for (let page of PAGES) {
