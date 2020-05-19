390e85df010325cbdc905f64f03da7bdd09328ee
Takuma <takumab@gmail.com>
2020-05-19 14:18:44 +0100

    Refactor name of language preview text

--- src/server/getDescription.js
+++ src/server/getDescription.js
@@ -1,13 +1,17 @@
 const config = require('./config');
-const ENGLISH_SNIPPET_TEXT = "Our software delivery assessment measures the current level of maturity of your software development organisation across 5 areas & includes recommendations for improvements.";
-const SPANISH_SNIPPET = "Nuestra evaluación de entrega de software permite medir el nivel actual de madurez de tu organización de desarrollo de software en cinco áreas distintas e incluye recomendaciones para aplicar posibles mejoras.";
+const ENGLISH_PREVIEW_TEXT = "Our software delivery assessment measures the " +
+    "current level of maturity of your software development organisation across " +
+    "5 areas & includes recommendations for improvements.";
+const SPANISH_PREVIEW_TEXT = "Nuestra evaluación de entrega de software permite medir" +
+    " el nivel actual de madurez de tu organización de desarrollo de software en " +
+    "cinco áreas distintas e incluye recomendaciones para aplicar posibles mejoras.";
 
 function isEnglishVersion() {
     return config.language !== "ES";
 }
 
 function getDescription() {
-    return isEnglishVersion() ? ENGLISH_SNIPPET_TEXT : SPANISH_SNIPPET;
+    return isEnglishVersion() ? ENGLISH_PREVIEW_TEXT : SPANISH_PREVIEW_TEXT;
 }
 
 module.exports = getDescription;
