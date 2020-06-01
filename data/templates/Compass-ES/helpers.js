979b3aadc29717c7326db3d58252912c6a66c1ab
Florian Kempenich <Flori@nKempenich.com>
2020-06-01 15:44:01 +0100

    Remove unused helper function

--- data/templates/Compass-ES/helpers.js
+++ data/templates/Compass-ES/helpers.js
@@ -1,8 +1,4 @@
 /* eslint-disable */
-function currentYear() {
-  return new Date().getFullYear();
-}
-
 Handlebars.registerHelper("scoreClass", function (score) {
   if (score > 75) return "good";
   if (score >= 50) return "avg";
