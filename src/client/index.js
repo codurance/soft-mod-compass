9135b5397848ce48af58baf138b4c18090429902
Takuma <takumab@gmail.com>
2020-06-02 10:25:31 +0100

    Add Prettier and ESLint

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,7 @@
-const generateUuid = require('uuid/v4')
+const generateUuid = require('uuid/v4');
 
-const surveyElement = document.getElementById('typeform-survey')
-
-const uuid = generateUuid()
+const surveyElement = document.getElementById('typeform-survey');
+const uuid = generateUuid();
 /*
 Hidden fields are pre-populated using query string parameters in both Typeform and Hubspot
 
@@ -11,9 +10,9 @@ Typeform: https://developer.typeform.com/embed/hidden-fields/
 Hubspot: https://knowledge.hubspot.com/forms/can-i-auto-populate-form-fields-through-a-query-string
 */
 
-const hiddenFieldToAutoPopulate = `uuid=${uuid}`
-const typeformSurveyUrlWithUuidAsHiddenField = `${surveyElement.dataset.typeformurl}/to/${surveyElement.dataset.typeformformid}?${hiddenFieldToAutoPopulate}`
-const hubspotLandingPageWithUuidAsHiddenField = `${surveyElement.dataset.hubspotformlandingpageurl}?${hiddenFieldToAutoPopulate}`
+const hiddenFieldToAutoPopulate = `uuid=${uuid}`;
+const typeformSurveyUrlWithUuidAsHiddenField = `${surveyElement.dataset.typeformurl}/to/${surveyElement.dataset.typeformformid}?${hiddenFieldToAutoPopulate}`;
+const hubspotLandingPageWithUuidAsHiddenField = `${surveyElement.dataset.hubspotformlandingpageurl}?${hiddenFieldToAutoPopulate}`;
 
 typeformEmbed.makeWidget(
   surveyElement,
@@ -21,20 +20,23 @@ typeformEmbed.makeWidget(
   {
     onSubmit: () => {
       setTimeout(() => {
-        window.location = hubspotLandingPageWithUuidAsHiddenField
+        window.location = hubspotLandingPageWithUuidAsHiddenField;
       }, 2500);
-    }
+    },
   }
-)
+);
 
-function checkSurveyHasFocus () {
-  const surveyIframe = document.getElementsByTagName('iframe')[0]
-  const iframeHasFocus = document.activeElement === surveyIframe
+function checkSurveyHasFocus() {
+  const surveyIframe = document.getElementsByTagName('iframe')[0];
+  const iframeHasFocus = document.activeElement === surveyIframe;
   if (iframeHasFocus) {
-    clearInterval(checkSurveyHasFocusInterval)
+    clearInterval(checkSurveyHasFocusInterval);
   } else {
-    surveyIframe.focus()
+    surveyIframe.focus();
   }
 }
 
-const checkSurveyHasFocusInterval = window.setInterval(checkSurveyHasFocus, 100)
+const checkSurveyHasFocusInterval = window.setInterval(
+  checkSurveyHasFocus,
+  100
+);
