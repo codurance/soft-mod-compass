4f7e5bb559cbfcb5e8cbda07a670f5dfa9ebab72
Cherry Tsui <cherry.tsui@codurance.com>
2020-06-02 11:10:59 +0100

    Refactor automation tests

--- cypress/integration/compass_e2e.js
+++ cypress/integration/compass_e2e.js
@@ -1,43 +1,48 @@
 describe('Compass flow', () => {
-    it('redirects to HubSpot Successful Submission page when completed', () => {
 
+    it('(EN) redirects to HubSpot Successful Submission page when completed', () => {
         cy.visit('https://compass-en.codurance.io')
-        assertStartButtonContainsCorrectTextAndClick()
-        completeTypeFormSurveryAndSubmit()
-        assertRedirectsToHubSpotLPAndContainsCorrectText()
-        fillInHubSpotSubmissionForm()
-        assertRedirectsToSucessfulSubmissionPageAndContainsCorrectText()
-
-        function assertStartButtonContainsCorrectTextAndClick() {
-            const startText = 'Start'
-            cy.assertStartButtonContainsCorrectText(startText)
-            cy.iframe().contains(startText).click()
-        }
-
-        function completeTypeFormSurveryAndSubmit() {
-            const waitTime = 1000
-            Array.from({length: 20}, (_x, i) => {
-                let answer = 'Strongly Agree'
-                if(i === 4) answer = 'Hourly'
-                cy.waitAndClickAnswer(waitTime, answer)
-            })
-            cy.waitAndClickAnswer(0, 'Submit')
-        }     
-
-        function assertRedirectsToHubSpotLPAndContainsCorrectText() {
-            cy.get('h2').should('contain.text', 'Receive your report')
-        }
-
-        function fillInHubSpotSubmissionForm() {
-            cy.get('[name=firstname]').type('Test')
-            cy.get('[name=lastname]').type('Test')
-            cy.get('[name=company]').type('Codurance')
-            cy.get('[name=email]').type('compass-test@codurance.com')
-            cy.contains('Submit').click()
-        }
-
-       function assertRedirectsToSucessfulSubmissionPageAndContainsCorrectText() {
-        cy.get('h1').should('contain.text', 'Success!')
-       }
+        assertStartPageIsInCorrectLanguage('Our software delivery assessment measures the current')
+        assertStartButtonContainsCorrectTextAndClick('Start')
+        completeTypeFormSurveryAndSubmit('Strongly Agree', 'Hourly', 'Submit')
+        assertRedirectsToHubSpotLPAndContains('Receive your report')
+        fillInHubSpotSubmissionFormAndSubmit()
+        assertRedirectsToSucessfulSubmissionPageAndContains('Success!')
     })
+
+    function assertStartPageIsInCorrectLanguage(text) {
+        cy.iframe().contains(text)
+    }
+
+    function assertStartButtonContainsCorrectTextAndClick(startText) {
+        cy.assertStartButtonContainsCorrectText(startText)
+        cy.iframe().contains(startText).click()
+    }
+
+    function completeTypeFormSurveryAndSubmit(answerText, q5AnswerText, submitText) {
+        const waitTime = 1000
+        Array.from({length: 20}, (_x, i) => {
+            let answer = answerText
+            if(i === 4) answer = q5AnswerText
+            cy.waitAndClickAnswer(waitTime, answer)
+        })
+        cy.waitAndClickAnswer(0, submitText)
+    }     
+
+    function assertRedirectsToHubSpotLPAndContains(headerText) {
+        cy.get('h2').should('contain.text', headerText)
+    }
+
+    function fillInHubSpotSubmissionFormAndSubmit() {
+        cy.get('[name=firstname]').type('Test')
+        cy.get('[name=lastname]').type('Test')
+        cy.get('[name=company]').type('Codurance')
+        cy.get('[name=email]').type('compass-test@codurance.com')
+        cy.get('.hs-button').click()
+        cy.wait(6000)
+    }
+
+   function assertRedirectsToSucessfulSubmissionPageAndContains(successText) {
+        cy.get('h1').should('contain.text', successText)
+   }
 })
