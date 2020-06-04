04bac1b68a0344065a490ed04abb34eaf454893a
Florian Kempenich <Flori@nKempenich.com>
2020-06-04 17:46:00 +0100

    Inject background images via CSS instead of hardcoding in HTML (#21)
    * Inject background images via CSS instead of hardcoding in HTML
    
    * Remove unused helper function

--- data/templates/Compass-ES/helpers.js
+++ data/templates/Compass-ES/helpers.js
@@ -1,33 +1,7 @@
 /* eslint-disable */
-function currentYear () {
-
-  return new Date().getFullYear()
-}
-Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-
-    switch (operator) {
-        case '==':
-            return (v1 == v2) ? options.fn(this) : options.inverse(this);
-        case '===':
-            return (v1 === v2) ? options.fn(this) : options.inverse(this);
-        case '!=':
-            return (v1 != v2) ? options.fn(this) : options.inverse(this);
-        case '!==':
-            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
-        case '<':
-            return (v1 < v2) ? options.fn(this) : options.inverse(this);
-        case '<=':
-            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-        case '>':
-            return (v1 > v2) ? options.fn(this) : options.inverse(this);
-        case '>=':
-            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-        case '&&':
-            return (v1 && v2) ? options.fn(this) : options.inverse(this);
-        case '||':
-            return (v1 || v2) ? options.fn(this) : options.inverse(this);
-        default:
-            return;
-    }
+Handlebars.registerHelper("scoreClass", function (score) {
+  if (score > 75) return "good";
+  if (score >= 50) return "avg";
+  return "bad";
 });
 /* eslint-enable */
