a17312410589b0fa09ecbc0a7046ae3179494b3e
cherryt <cherry.tsui@codurance.com>
2020-06-15 10:18:58 +0100

    E2E tests (#24)
    * add first cypress test
    
    * Add automation to complete TypeForm Survey
    
    * e2e tests automated up until Successful Submission page
    
    * Refactor automation tests
    
    * Update gitignore
    
    * Find start button using data-attribute
    
    * run linter
    
    * use then instead of wait

--- cypress/integration/typeform_survey.spec.js
+++
@@ -1,5 +0,0 @@
-describe('TypeForm Survey', ()=>{
-    it('directs to TypeForm', () => {
-        cy.visit('https://compass-en.codurance.io');
-    })
-})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('waitAndClickAnswer', (waitTime, answer) => {
+  cy.wait(waitTime);
+  cy.iframe().find('section[data-qa-focused=true]').contains(answer).click();
+});
