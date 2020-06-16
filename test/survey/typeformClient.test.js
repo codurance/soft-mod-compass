dcf526be413a2b3940745e9c7673f610e9950e8f
Florian Kempenich <Flori@nKempenich.com>
2020-06-16 12:28:08 +0100

    Wrap in describe blocks

--- test/survey/typeformClient.test.js
+++ test/survey/typeformClient.test.js
@@ -13,95 +13,100 @@ const typeformClient = require('../../src/server/survey/typeformClient')(
 );
 
 describe('typeformClient', () => {
-  const mockUuid = 'MOCK_UUID';
-  const queryAnswersForMockUuidUrl = `/forms/${config.typeform.formId}/responses?query=${mockUuid}`;
   const OK = 200;
-  const answerEmpty = { items: [] };
-  const answerWithTwoItems = {
-    items: [
-      {
-        answers: [{ choice: { label: 'one' } }, { choice: { label: 'two' } }],
-      },
-    ],
-  };
-  const expectedTwoItems = ['one', 'two'];
 
-  afterEach(() => {
-    nock.cleanAll();
+  describe('get survey answers', () => {
+    const mockUuid = 'MOCK_UUID';
+    const queryAnswersForMockUuidUrl = `/forms/${config.typeform.formId}/responses?query=${mockUuid}`;
+    const answerEmpty = { items: [] };
+    const answerWithTwoItems = {
+      items: [
+        {
+          answers: [{ choice: { label: 'one' } }, { choice: { label: 'two' } }],
+        },
+      ],
+    };
+    const expectedTwoItems = ['one', 'two'];
+
+    afterEach(() => {
+      nock.cleanAll();
+    });
+
+    it('successfuly get an extract result', (done) => {
+      nock(config.typeform.url)
+        .get(queryAnswersForMockUuidUrl)
+        .reply(OK, answerWithTwoItems);
+
+      typeformClient
+        .surveyAnswersFor(mockUuid)
+        .then((res) => {
+          expect(res).toEqual(expectedTwoItems);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('retries multiple times if answers are empty', (done) => {
+      nock(config.typeform.url)
+        .get(queryAnswersForMockUuidUrl)
+        .reply(OK, answerEmpty)
+        .get(queryAnswersForMockUuidUrl)
+        .reply(OK, answerEmpty)
+        .get(queryAnswersForMockUuidUrl)
+        .reply(OK, answerWithTwoItems);
+
+      typeformClient
+        .surveyAnswersFor(mockUuid)
+        .then((res) => {
+          expect(res).toEqual(expectedTwoItems);
+          done();
+        })
+        .catch(done);
+    });
+
+    it('retries no more than the given parameter', async () => {
+      const expectedRetries = 3;
+
+      nock(config.typeform.url)
+        .get(queryAnswersForMockUuidUrl)
+        .times(expectedRetries)
+        .reply(OK, answerEmpty);
+
+      try {
+        await typeformClient.surveyAnswersFor(mockUuid, expectedRetries);
+        fail('did not throw');
+      } catch (err) {
+        expect(err.message).toEqual(
+          `no survey answers for ${mockUuid}` // TODO: Re-introduce number of retries in assertion
+        );
+      }
+    });
   });
 
-  it('get survey answers for a particular uuid', (done) => {
-    nock(config.typeform.url)
-      .get(queryAnswersForMockUuidUrl)
-      .reply(OK, answerWithTwoItems);
+  describe('get survey schema', () => {
+    it('sucessfully gets and and extract schema', (done) => {
+      const questions = [
+        { properties: { choices: choicesData() } },
+        { properties: { choices: choicesData() } },
+      ];
 
-    typeformClient
-      .surveyAnswersFor(mockUuid)
-      .then((res) => {
-        expect(res).toEqual(expectedTwoItems);
-        done();
-      })
-      .catch(done);
-  });
+      nock(config.typeform.url)
+        .get(`/forms/${config.typeform.formId}`)
+        .reply(OK, {
+          fields: questions,
+        });
 
-  it('retries getting survey answers if answers are empty', (done) => {
-    nock(config.typeform.url)
-      .get(queryAnswersForMockUuidUrl)
-      .reply(OK, answerEmpty)
-      .get(queryAnswersForMockUuidUrl)
-      .reply(OK, answerEmpty)
-      .get(queryAnswersForMockUuidUrl)
-      .reply(OK, answerWithTwoItems);
-
-    typeformClient
-      .surveyAnswersFor(mockUuid)
-      .then((res) => {
-        expect(res).toEqual(expectedTwoItems);
-        done();
-      })
-      .catch(done);
-  });
-
-  it('retries getting survey answers no more than the given parameter', async () => {
-    const expectedRetries = 3;
-
-    nock(config.typeform.url)
-      .get(queryAnswersForMockUuidUrl)
-      .times(expectedRetries)
-      .reply(OK, answerEmpty);
-
-    try {
-      await typeformClient.surveyAnswersFor(mockUuid, expectedRetries);
-      fail('did not throw');
-    } catch (err) {
-      expect(err.message).toEqual(
-        `no survey answers for ${mockUuid}` // TODO: Re-introduce number of retries in assertion
-      );
-    }
-  });
-
-  it('gets survey schema from typeform', (done) => {
-    const questions = [
-      { properties: { choices: choicesData() } },
-      { properties: { choices: choicesData() } },
-    ];
-
-    nock(config.typeform.url)
-      .get(`/forms/${config.typeform.formId}`)
-      .reply(OK, {
-        fields: questions,
-      });
-
-    typeformClient
-      .getQuestionChoices()
-      .then((questions) => {
-        expect(questions).toEqual([
-          ['one', 'two', 'three'],
-          ['one', 'two', 'three'],
-        ]);
-        done();
-      })
-      .catch(done);
+      typeformClient
+        .getQuestionChoices()
+        .then((questions) => {
+          expect(questions).toEqual([
+            ['one', 'two', 'three'],
+            ['one', 'two', 'three'],
+          ]);
+          done();
+        })
+        .catch(done);
+    });
   });
 });
 
