947b9d5e252b552303cd49ffd87cfcc27f265e05
Florian Kempenich <Flori@nKempenich.com>
2020-06-25 16:02:23 +0100

    Extract typeform client logic to separate module

--- src/client/index.js
+++ src/client/index.js
@@ -1,63 +1,15 @@
-const generateUuid = require('uuid/v4');
-const typeformEmbed = require('@typeform/embed');
 const cookieMessageFactory = require('./cookieMessage');
 const hubspotEmbeddedFactory = require('./hubspotEmbedded');
+const typeformFactory = require('./typeform');
 
-/*
-Hidden fields are pre-populated using query string parameters in both Typeform and Hubspot
-
-Sources
-Typeform: https://developer.typeform.com/embed/hidden-fields/
-Hubspot: https://knowledge.hubspot.com/forms/can-i-auto-populate-form-fields-through-a-query-string
-*/
-const uuid = generateUuid();
-const surveyElement = document.getElementById('typeform-survey');
-const hiddenFieldToAutoPopulate = `uuid=${uuid}`;
-const typeformSurveyUrlWithUuidAsHiddenField = `${surveyElement.dataset.typeformurl}/to/${surveyElement.dataset.typeformformid}?${hiddenFieldToAutoPopulate}`;
-const hubspotLandingPageWithUuidAsHiddenField = `${surveyElement.dataset.hubspotformlandingpageurl}?${hiddenFieldToAutoPopulate}`;
-
-typeformEmbed.makeWidget(
-  surveyElement,
-  typeformSurveyUrlWithUuidAsHiddenField,
-  {
-    onSubmit: () => {
-      setTimeout(() => {
-        window.location = hubspotLandingPageWithUuidAsHiddenField;
-      }, 2500);
-    },
-  }
-);
-
-function checkSurveyHasFocus() {
-  const surveyIframe = document.getElementsByTagName('iframe')[0];
-  const iframeHasFocus = document.activeElement === surveyIframe;
-  if (iframeHasFocus) {
-    clearInterval(checkSurveyHasFocusInterval);
-  } else {
-    surveyIframe.focus();
-  }
-}
-
-const checkSurveyHasFocusInterval = window.setInterval(
-  checkSurveyHasFocus,
-  100
-);
-
-window.ready = ready;
-
-function ready(func) {
-  if (document.readyState === 'complete' || document.readyState != 'loading') {
-    func();
-  } else {
-    document.addEventListener('DOMContentLoaded', func);
-  }
-}
-
-window.compass = window.compass || {};
-ready(function () {
+const onReady = () => {
   const cookieMessage = cookieMessageFactory();
-  window.compass.cookieMessage = cookieMessage;
+  const hubspotEmbedded = hubspotEmbeddedFactory(cookieMessage);
+  const typeform = typeformFactory();
 
-  hubspotEmbedded = hubspotEmbeddedFactory(cookieMessage);
   hubspotEmbedded.initializeHubspot();
-});
+  typeform.initializeTypeformSurvey();
+  typeform.ensureSurveyAlwaysHasFocus();
+};
+
+document.addEventListener('DOMContentLoaded', onReady);
