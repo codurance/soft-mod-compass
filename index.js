5ff3675b11b6e5bf9b4bf474fbb71e7457af0507
Florian Kempenich <Flori@nKempenich.com>
2020-07-08 17:39:41 +0100

    Apply prettier on 'index.js'

--- index.js
+++ index.js
@@ -1,73 +1,77 @@
-const express = require('express')
-const reportingApp = express()
+const express = require('express');
+const reportingApp = express();
 
-const config = require('./src/server/config')
-const typeformClient = require('./src/server/survey/typeformClient')(config)
-const getHubspotUserDetails = require('./src/server/report/getHubspotUserDetails')
+const config = require('./src/server/config');
+const typeformClient = require('./src/server/survey/typeformClient')(config);
+const getHubspotUserDetails = require('./src/server/report/getHubspotUserDetails');
 const reportViewModelBuilder = require('./src/server/report/reportViewModelBuilder')(
   typeformClient,
   getHubspotUserDetails
-)
-const app = require('./src/server/app')(config, reportingApp, reportViewModelBuilder)
+);
+const app = require('./src/server/app')(
+  config,
+  reportingApp,
+  reportViewModelBuilder
+);
 
-const port = 8080
+const port = 8080;
 
 const server = app.listen(port, () => {
-  console.log(`ready at http://localhost:${port}`)
-})
+  console.log(`ready at http://localhost:${port}`);
+});
 
 const jsreport = require('jsreport')({
   store: {
-    provider: 'fs'
+    provider: 'fs',
   },
   extensions: {
     express: {
       app: reportingApp,
-      server: app
+      server: app,
     },
     'fs-store': {
       dataDirectory: 'data',
       syncModifications: true,
       sync: {
-        usePolling: true
-      }
+        usePolling: true,
+      },
     },
     templatingEngines: {
       strategy: 'dedicated-process',
       timeout: 40000,
-      numberOfWorkers: 2
+      numberOfWorkers: 2,
     },
     'chrome-pdf': {
       timeout: 20000,
       strategy: 'chrome-pool',
       numberOfWorkers: 2,
       launchOptions: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-      }
-    }
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      },
+    },
   },
-  appPath: '/reporting'
-})
+  appPath: '/reporting',
+});
 
 jsreport
   .init()
   .then(() => {
-    console.log('jsreport server started')
+    console.log('jsreport server started');
   })
-  .catch(e => {
-    console.error(e)
-  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 process.on('SIGTERM', function () {
   server.close(() => {
-    console.log('Received SIGTERM, shutting down')
-    process.exit(0)
-  })
-})
+    console.log('Received SIGTERM, shutting down');
+    process.exit(0);
+  });
+});
 
 process.on('SIGINT', function () {
-  console.log('Received SIGINT, shutting down')
+  console.log('Received SIGINT, shutting down');
   server.close(() => {
-    process.exit(0)
-  })
-})
+    process.exit(0);
+  });
+});
