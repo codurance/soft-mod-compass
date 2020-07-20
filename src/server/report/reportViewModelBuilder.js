b4ec5f6a16e74b41a584b8bd4c5c483a609e500d
Florian Kempenich <Flori@nKempenich.com>
2020-07-20 17:26:08 +0100

    Add logging

--- src/server/report/reportViewModelBuilder.js
+++ src/server/report/reportViewModelBuilder.js
@@ -5,7 +5,9 @@ const typeformClient = require('../survey/typeformClient');
 
 async function buildReportViewModelFor(submissionUuid) {
   const choices = await typeformClient.getQuestionChoices();
+  console.log(`Fetching survey answers from Typeform for '${submissionUuid}'`);
   const answers = await typeformClient.surveyAnswersFor(submissionUuid);
+  console.log(`Fetching user details from Hubspot for '${submissionUuid}'`);
   const userDetails = await getHubspotUserDetails(submissionUuid);
 
   console.log(`User details for '${submissionUuid}'`, userDetails);
