17deb7526548da02b2fc9eac11c96a6dea76f30a
Takuma <takumab@gmail.com>
2020-07-29 15:41:32 +0100

    Refactor config to remove unnecessary imports

--- cypress/integration/hubspot/compassEn.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Compass on Hubspot', function () {
-  it('should visit first page of Compass and click start button', function () {
-    cy.visit('https://info.codurance.com/dev-compass-en-survey-landing-page');
-    cy.iframe().find('[data-qa=start-button]').click();
-  });
-});
