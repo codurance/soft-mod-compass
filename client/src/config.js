53bf53a6fbf8d11b3338a8f2d435237d97116b41
Florian Kempenich <Flori@nKempenich.com>
2020-08-07 15:40:51 +0100

    Use Hubspot to host social media preview info

--- client/src/config.js
+++ client/src/config.js
@@ -1,31 +1,24 @@
-const ENGLISH_TITLE = 'Compass by Codurance | Assessment Tool';
-const SPANISH_TITLE = 'Compass by Codurance | Herramienta de evaluación';
-
 module.exports = {
   devEn: {
     filename: 'compass-survey-dev-en.html',
     typeformFormId: 'j0ytULD0',
     hubspotFormUrl:
       'https://info.codurance.com/en/dev-compass-details-submission',
-    title: ENGLISH_TITLE,
   },
   devEs: {
     filename: 'compass-survey-dev-es.html',
     typeformFormId: 'AQRUSE',
     hubspotFormUrl:
       'https://info.codurance.com/es/dev-compass-details-submission',
-    title: SPANISH_TITLE,
   },
   prodEn: {
     filename: 'compass-survey-prod-en.html',
     typeformFormId: 'gF83u4IR',
     hubspotFormUrl: 'https://info.codurance.com/en/compass-details-submission',
-    title: ENGLISH_TITLE,
   },
   prodEs: {
     filename: 'compass-survey-prod-es.html',
     typeformFormId: 'ZoBrCZ',
     hubspotFormUrl: 'https://info.codurance.com/es/compass-details-submission',
-    title: SPANISH_TITLE,
   },
 };
