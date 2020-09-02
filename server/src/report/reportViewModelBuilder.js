f25c3ba4af84e769a49c19d7ab414387b9ac55f1
Florian Kempenich <Flori@nKempenich.com>
2020-09-02 17:39:05 +0100

    Use Hubspot Api in place of 'getHubspotUserDetails'

--- server/src/report/reportViewModelBuilder.js
+++ server/src/report/reportViewModelBuilder.js
@@ -1,15 +1,15 @@
 const reportViewModel = require('./reportViewModel');
 const categoryDefinitions = require('./categoryDefinitions');
-const getHubspotUserDetails = require('./getHubspotUserDetails');
 const typeformClient = require('../survey/typeformClient');
 const roundCategoryScoresToNearestQuarter = require('./roundCategoryScoresToNearestQuarter');
+const hubspotApi = require('./hubspot/api');
 
 async function buildReportViewModelFor(submissionUuid) {
   const choices = await typeformClient.getQuestionChoices();
   console.log(`Fetching survey answers from Typeform for '${submissionUuid}'`);
   const answers = await typeformClient.surveyAnswersFor(submissionUuid);
   console.log(`Fetching user details from Hubspot for '${submissionUuid}'`);
-  const userDetails = await getHubspotUserDetails(submissionUuid);
+  const userDetails = await hubspotApi.getFormSubmission(submissionUuid);
 
   console.log(`User details for '${submissionUuid}'`, userDetails);
 
