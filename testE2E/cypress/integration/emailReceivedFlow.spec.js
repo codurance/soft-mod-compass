6eeeb8fb2d10d9393d10954773263c7f597ec01b
Takuma <takumab@gmail.com>
2020-10-14 16:36:02 +0100

    Set up tests for mobbing

--- testE2E/cypress/integration/emailReceivedFlow.spec.js
+++ testE2E/cypress/integration/emailReceivedFlow.spec.js
@@ -4,4 +4,12 @@ context('Email Received', () => {
     const data = JSON.parse(inbox);
     expect(data.result).to.equal('success');
   });
+
+  it('should contain report link to download', async () => {});
+
+  it('should return downloaded version of report', function () {});
+
+  it('should return converted pdf as image', function () {});
+
+  it('should compare the image and return percentage difference', function () {});
 });
