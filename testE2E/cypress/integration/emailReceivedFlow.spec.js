e2fc8ddfd28acb3978118e9697b3154d03cff1b4
Takuma <takumab@gmail.com>
2020-10-14 15:46:15 +0100

    Remove unnecessary test

--- testE2E/cypress/integration/emailReceivedFlow.spec.js
+++ testE2E/cypress/integration/emailReceivedFlow.spec.js
@@ -4,10 +4,4 @@ context('Email Received', () => {
     const data = JSON.parse(inbox);
     expect(data.result).to.equal('success');
   });
-
-  it('should contain english text', async () => {
-    const inbox = await cy.task('queryTestmail');
-    const data = JSON.parse(inbox);
-    expect(data.emails.length).to.equal(2);
-  });
 });
