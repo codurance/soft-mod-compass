d5941d00dc74f6ea259d094925d791e36d432fde
nicolasbelfis <nicolas.belfis@codurance.com>
2021-01-14 15:16:40 +0100

    configure acceptance test

--- client/src/App.test.jsx
+++ client/src/App.test.jsx
@@ -1,46 +1,57 @@
 import { render } from '@testing-library/react';
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 import {
-  beforeEach, describe, it, jest,
+  describe, it, jest, expect,
 } from '@jest/globals';
+import nock from 'nock';
+import reportService from './services/reportService';
 import App from './App';
+import payloadRequest from './mockdata/post_survey_request_body.json';
 
-const payloadRequest = { data: '' };
-
+const userFirstName = 'FIRSTNAME';
+const userLastName = 'LASTNAME';
+const userCompany = 'COMPANY';
+const userEmail = 'user@mail.com';
+const successfulResponseFromBackend = { status: 'ok' };
+const REPORT_BACKEND_URL = 'compass.codurance.io';
+const SUBMIT_SURVEY_URI = '/surveys';
 /*
 Given a user load compass survey
 When the user choose "Strongly Agree" in the question
 And the user fill his details (names, company, email)
 And the user hits submit
 Then the following POST request should be sent (look json file attached)
+Then the user is redirected to thank you page
  */
 describe('acceptance test', () => {
-  const submitSurvey = jest.fn();
-  jest.doMock('./services/reportService', () => ({
-    submitSurvey,
-  }));
-  const reportService = require('./services/reportService');
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+  nock(REPORT_BACKEND_URL)
+    .post(SUBMIT_SURVEY_URI, payloadRequest)
+    .reply(201);
 
-  it('loads and displays greeting', () => {
-    // jest.spyOn(reportService, 'submitSurvey');
-    // failure Promise.reject(new Error('fail'))
-    // success new Promise((resolve) => resolve(getMockResponse()));
-
-    submitSurvey.mockReturnValue(1);
-
-    const returnedValue = reportService.submitSurvey(payloadRequest);
-    console.log('returned value ', returnedValue);
-    const app = render(<App />);
-    // app.getByText("Hello World!")
-
+  it('should submit information about the survey', () => {
+    const { getByText, getByLabelText } = render(<App />);
     // when I fill the survey and click on submit
+    const answerChoice = getByText('Strongly Agree');
+    fireEvent.click(answerChoice);
+
+    const inputFirstName = getByLabelText('firstName');
+    fireEvent.change(inputFirstName, { target: { value: userFirstName } });
+
+    const inputLastName = getByLabelText('lastName');
+    fireEvent.change(inputLastName, { target: { value: userLastName } });
+
+    const inputCompany = getByLabelText('company');
+    fireEvent.change(inputCompany, { target: { value: userCompany } });
+
+    const inputEmail = getByLabelText('email');
+    fireEvent.change(inputEmail, { target: { value: userEmail } });
+
+    const button = getByText('submit');
+    fireEvent.click(button);
 
     // then
-    // expect(reportService.submitSurvey).toBeCalledWith(payloadRequest)
-    app.getByText('Thank you for submit compass !');
-    app.getByText('an error occurred !');
+    const spy = jest.spyOn(reportService, 'submitSurvey');
+    expect(spy).toReturnWith(Promise.resolve(successfulResponseFromBackend));
   });
 });
