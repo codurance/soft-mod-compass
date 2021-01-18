559b4e9725b5818b6c44dc19a9e3676d879a84a2
nicolasbelfis <nicolas.belfis@codurance.com>
2021-01-19 00:05:07 +0100

    fix eslint issues

--- client/src/components/AnswerButton.jsx
+++ client/src/components/AnswerButton.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function AnswerButton({ answer, selectedAnswer, clickCallback }) {
-
-
   return (
     <button
       type="button"
