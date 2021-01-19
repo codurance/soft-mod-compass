27682c690b8944cfe2319e089af73618dfb293c1
martin-garcia-blanco <programadopormartin@gmail.com>
2021-01-19 19:32:12 +0100

    WIP Welcome and Button styles

--- client/src/components/Welcome/Welcome.jsx
+++ client/src/components/Welcome/Welcome.jsx
@@ -2,14 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import translator from '../../config/translator';
 import Button from '../Button/Button';
+import './styles.scss';
 
 function Welcome({ clickCallback }) {
   return (
-    <>
-      <p>{translator.welcomeFirstParagraph}</p>
-      <p>{translator.welcomeSecondParagraph}</p>
+    <section className="welcome">
+      <img
+        src="https://images.typeform.com/images/BQieVdsGyDW3/image/default"
+        alt="Compass by Codurance"
+      />
+
+      <div className="welcome__text">
+        <p>{translator.welcomeFirstParagraph}</p>
+        <p>{translator.welcomeSecondParagraph}</p>
+      </div>
       <Button label="Start" clickCallback={clickCallback} />
-    </>
+    </section>
   );
 }
 
