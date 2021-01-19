7bbfe3766a0a9ed2b72e2d8802039510ac7b9d9e
martin-garcia-blanco <programadopormartin@gmail.com>
2021-01-19 11:35:14 +0100

    Add onClick callback prop in Welcome component

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import translator from '../config/translator';
 import Button from './Button';
+import PropTypes from 'prop-types';
 
-function Welcome() {
+function Welcome({ clickCallback }) {
   return (
     <>
       <p>{translator.welcomeFirstParagraph}</p>
       <p>{translator.welcomeSecondParagraph}</p>
-      <Button label="Start" clickCallback={() => {}} />
+      <Button label="Start" clickCallback={clickCallback} />
     </>
   );
 }
+
+Welcome.propTypes = {
+  clickCallback: PropTypes.func.isRequired,
+};
 
 export default Welcome;
--- client/src/components/welcome.test.jsx
+++ client/src/components/welcome.test.jsx
@@ -6,10 +6,19 @@ import {
 } from '../config/en-labels.json';
 import Welcome from './Welcome';
 
+const clickCallback = jest.fn();
+
 describe('Welcome', () => {
   it('display 2 paragraphs', () => {
-    const { getByText } = render(<Welcome />);
+    const { getByText } = render(<Welcome clickCallback={() => {}} />);
     expect(getByText(welcomeFirstParagraph)).toBeInTheDocument();
     expect(getByText(welcomeSecondParagraph)).toBeInTheDocument();
+  });
+
+  it('should execute the callback when the user clicks in the button', () => {
+    const { getByText } = render(<Welcome clickCallback={clickCallback} />);
+    getByText('Start').click();
+
+    expect(clickCallback).toHaveBeenCalledTimes(1);
   });
 });
