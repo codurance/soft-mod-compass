e36ff1b301631b8b89ddde2d4001ca28cc283b30
martin-garcia-blanco <programadopormartin@gmail.com>
2021-01-19 17:16:02 +0100

    Sass setup

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -35,6 +35,10 @@ module.exports = (env) => {
           test: /\.css$/,
           use: ['style-loader', 'css-loader'],
         },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+        },
       ],
     },
     mode: 'development',
