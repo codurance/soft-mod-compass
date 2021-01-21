ce03e1e66a455a00b9c97a8845c3602abbf7ac2b
nicolasbelfis <nicolas.belfis@codurance.com>
2021-01-21 18:55:28 +0100

    config CI/CD for backend

--- backend/server/src/config.js
+++ backend/server/src/config.js
@@ -1,15 +1,12 @@
 const environmentIsDevelopment = process.env.NODE_ENV !== 'production' || false;
-const isESVersion = () => process.env.COMPASS_LANGUAGE === 'ES';
 
 module.exports = {
-  isESVersion: isESVersion(),
   jsreport: {
     studioEditorEnabled: environmentIsDevelopment,
   },
   hubspot: {
     formId: process.env.HUBSPOT_FORM_ID,
     authToken: process.env.HUBSPOT_AUTH_TOKEN,
-    thanksLandingPageUrl: process.env.HUBSPOT_THANKS_LANDING_PAGE_URL,
     portalId: process.env.HUBSPOT_ACCOUNT_ID,
   },
   app: {
