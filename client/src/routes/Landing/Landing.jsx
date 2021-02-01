64bd54072d83c0f5b8951c69b6772f6100c0ee12
martin-garcia-blanco <programadopormartin@gmail.com>
2021-02-01 18:41:04 +0100

    WIP landing styles

--- client/src/routes/Landing/Landing.jsx
+++ client/src/routes/Landing/Landing.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import translator from '../../config/translator';
+import CoduranceLogo from '../../assets/icons/logo-codurance.svg';
+import CompassLogo from '../../assets/icons/logo-compass.svg';
+import MaturityIcon from '../../assets/icons/icon-maturity.svg';
+import './styles.scss';
 
 function Landing() {
   const history = useHistory();
@@ -17,17 +21,45 @@ function Landing() {
   };
 
   return (
-    <>
-      <header>Icons</header>
+    <div className="landing">
+      <header className="landing__header">
+        <img src={CoduranceLogo} alt="Codurance" />
 
-      <main>
+        <span className="landing__header__separator" />
+        <img src={CompassLogo} alt="Compass" />
+      </header>
+
+      <main className="landing__main">
         <h1>{welcomeFirstParagraph}</h1>
         <p>{welcomeSecondParagraph}</p>
-        <div className="icons" />
+        <div className="landing__main__icons">
+          <div>
+            <img src={CompassLogo} alt="Compass" />
+            <p>Organisational Maturity</p>
+          </div>
+
+          <div>
+            <img src={CompassLogo} alt="Compass" />
+            <p>Organisational Maturity</p>
+          </div>
+          <div>
+            <img src={CompassLogo} alt="Compass" />
+
+            <p>Organisational Maturity</p>
+          </div>
+          <div>
+            <img src={CompassLogo} alt="Compass" />
+            <p>Organisational Maturity</p>
+          </div>
+          <div>
+            <img src={CompassLogo} alt="Compass" />
+            <p>Organisational Maturity</p>
+          </div>
+        </div>
         <p>{welcomeThirdParagraph}</p>
         <Button label={start} clickCallback={questionnaireRedirect} />
       </main>
-    </>
+    </div>
   );
 }
 
