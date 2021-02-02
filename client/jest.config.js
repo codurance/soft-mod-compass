74cf8b30cfd9b2bb85b155b38e341843d8b706fc
martin-garcia-blanco <programadopormartin@gmail.com>
2021-02-02 12:09:50 +0100

    Add dependency to transform svg files in tests

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.svg$': 'jest-svg-transformer',
+  },
 };
