7c69db3cca98aaaa10123115e7f729fd561f92e6
martin-garcia-blanco <programadopormartin@gmail.com>
2021-02-08 15:44:30 +0100

    Add animation when question appears

--- client/src/components/Questionnaire/Questionnaire.jsx
+++ client/src/components/Questionnaire/Questionnaire.jsx
@@ -1,16 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import Question from '../Question/Question';
 import './styles.scss';
 
 function Questionnaire({ currentQuestion, onClickAnswer, isSelectedAnswer }) {
   return (
     <div className="questionnaire">
-      <Question
-        question={currentQuestion}
-        onClickAnswer={onClickAnswer}
-        isSelectedFunction={isSelectedAnswer}
-      />
+      <TransitionGroup className="foo-wrapper">
+        <CSSTransition
+          timeout={1000}
+          appear
+          key={currentQuestion.label}
+          classNames="fade"
+        >
+          <Question
+            question={currentQuestion}
+            onClickAnswer={onClickAnswer}
+            isSelectedFunction={isSelectedAnswer}
+          />
+        </CSSTransition>
+      </TransitionGroup>
       <div
         className={`questionnaire__assessment--${currentQuestion.category}`}
         data-testid={`background-${currentQuestion.category}`}
