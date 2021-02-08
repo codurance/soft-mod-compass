9caaa019ac91bb5c49e6a6f25aad4cf7e58d68d2
martin-garcia-blanco <programadopormartin@gmail.com>
2021-02-08 12:30:48 +0100

    Translate userForm component

--- client/src/components/UserForm/UserForm.jsx
+++ client/src/components/UserForm/UserForm.jsx
@@ -6,45 +6,59 @@ import './styles.scss';
 import submissionImage from '../../assets/icons/report-submission-page.svg';
 import userFormValues from '../../config/userFormValues';
 
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+const {
+  userFormTitle,
+  userFormDescription,
+  firstName,
+  firstNameError,
+  lastName,
+  lastNameError,
+  companyName,
+  companyNameError,
+  jobFunctionError,
+  email,
+  emailError,
+  submit,
+  jobFunction,
+} = translator;
+
 function UserForm({ submitForm }) {
   const { register, errors, handleSubmit } = useForm();
   function onSubmit(data) {
     submitForm(data);
   }
 
-  const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   return (
     <div className="form">
       <div className="form__fields-wrapper">
-        <h2>{translator.userFormTitle}</h2>
-        <p>{translator.userFormDescription}</p>
+        <h2>{userFormTitle}</h2>
+        <p>{userFormDescription}</p>
         <form onSubmit={handleSubmit(onSubmit)}>
           <input
             className={`input${errors.firstName ? '__error' : ''}`}
-            placeholder={translator.firstName}
+            placeholder={firstName}
             name="firstName"
             ref={register({ required: true })}
           />
           <div className="form__error">
-            {errors.firstName && 'First name is required'}
+            {errors.firstName && firstNameError}
           </div>
           <input
             className={`input${errors.lastName ? '__error' : ''}`}
-            placeholder={translator.lastName}
+            placeholder={lastName}
             name="lastName"
             ref={register({ required: true })}
           />
-          <div className="form__error">
-            {errors.lastName && 'Last name is required'}
-          </div>
+          <div className="form__error">{errors.lastName && lastNameError}</div>
           <input
             className={`input${errors.companyName ? '__error' : ''}`}
-            placeholder={translator.companyName}
+            placeholder={companyName}
             name="companyName"
             ref={register({ required: true })}
           />
           <div className="form__error">
-            {errors.companyName && 'Company name is required'}
+            {errors.companyName && companyNameError}
           </div>
           <select
             name="jobFunction"
@@ -53,7 +67,7 @@ function UserForm({ submitForm }) {
             defaultValue={null}
             className={`input${errors.jobFunction ? '__error' : ''}`}
           >
-            <option data-testid="option" value={null} label="Job Function" />
+            <option data-testid="option" value={null} label={jobFunction} />
             {userFormValues.map((element) => (
               <option
                 data-testid="option"
@@ -64,22 +78,20 @@ function UserForm({ submitForm }) {
             ))}
           </select>
           <div className="form__error">
-            {errors.jobFunction && 'Job function is required'}
+            {errors.jobFunction && jobFunctionError}
           </div>
           <input
             className={`input${errors.email ? '__error' : ''}`}
-            placeholder={translator.email}
+            placeholder={email}
             name="email"
             ref={register({ required: true, pattern: emailRegex })}
           />
-          <div className="form__error">
-            {errors.email && 'valid email is required'}
-          </div>
+          <div className="form__error">{errors.email && emailError}</div>
           <div className="form__submit">
             <input
               className="button color-persimmon"
               type="submit"
-              value={translator.submit}
+              value={submit}
             />
           </div>
         </form>
