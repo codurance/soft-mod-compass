eb5b9961ae2f9eb02cce04d9ccac884b3de29da6
martin-garcia-blanco <programadopormartin@gmail.com>
2021-02-09 10:59:51 +0100

    Refactor linkedList to use the index

--- client/src/config/factory.js
+++ client/src/config/factory.js
@@ -5,23 +5,24 @@ module.exports = {
   }),
   createLinkedList(array) {
     const arrayCopy = array.slice();
-    function node(obj) {
+    function node(obj, index) {
       return {
         data: obj,
         next: undefined,
         previous: undefined,
+        index,
       };
     }
     const headElement = arrayCopy.shift();
 
     const linkedList = {
-      head: node(headElement),
+      head: node(headElement, 0),
       add(obj) {
         let tmp = this.head;
         while (tmp.next) {
           tmp = tmp.next;
         }
-        tmp.next = node(obj);
+        tmp.next = node(obj, tmp.index + 1);
         tmp.next.previous = tmp;
       },
     };
