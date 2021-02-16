53486e9504b7a58f45b01eb05dffd64923c2f948
martin-garcia-blanco <programadopormartin@gmail.com>
2021-02-16 21:35:04 +0100

    Test happy path of submitSurvey

---
+++ backend/server/src/survey/surveyOrchestrator.test.js
@@ -0,0 +1,40 @@
+const bodyRequest = require('../mockData/post_survey_request_body.json');
+
+const generateReportMock = jest.fn();
+jest.doMock('../jsreportAdapter', () => generateReportMock);
+const uploadReportToHubspotMock = {
+  uploadReportToHubspot: jest.fn(),
+  submitHubspotForm: jest.fn(),
+};
+jest.doMock(
+  '../report/hubspot/uploadToHubspot',
+  () => uploadReportToHubspotMock
+);
+const { submitSurvey } = require('./surveyOrchestrator');
+
+describe('SurveyOrchestrator', () => {
+  const pdfLinkMock = 'pdfLink';
+
+  it('process the survey', async () => {
+    generateReportMock.mockImplementation(() => ({ content: '' }));
+    uploadReportToHubspotMock.uploadReportToHubspot.mockImplementation(
+      () => pdfLinkMock
+    );
+    const expectedResult = {
+      status: 'ok',
+      pdfUrl: pdfLinkMock,
+    };
+    uploadReportToHubspotMock.submitHubspotForm.mockImplementation(() => {});
+
+    const result = await submitSurvey(bodyRequest);
+
+    expect(result).toEqual(expectedResult);
+    expect(generateReportMock).toHaveBeenCalledTimes(1);
+    expect(
+      uploadReportToHubspotMock.uploadReportToHubspot
+    ).toHaveBeenCalledTimes(1);
+    expect(uploadReportToHubspotMock.submitHubspotForm).toHaveBeenCalledTimes(
+      1
+    );
+  });
+});
