2ed85a8f12fb6eb8dad8fb681c5c0262ad5e866e
adrianmunoz <adrian.munoz@codurance.com>
2023-01-30 18:14:24 +0100

    Removed not needed comment from jest config file

--- client/jest.config.js
+++ client/jest.config.js
@@ -5,7 +5,6 @@ module.exports = {
     '^.+\\.svg$': 'jest-svg-transformer',
   },
   transformIgnorePatterns: [
-    // Change MODULE_NAME_HERE to your module that isn't being compiled
     '/node_modules/(?!public-ip|is-ip|ip-regex|indent-string|aggregate-error|clean-stack|escape-string-regexp).+\\.js$',
   ],
   moduleNameMapper: {
