575f64965b29baa67786a5fbdd519e34074f4217
adrianmunoz <adrian.munoz@codurance.com>
2023-01-30 19:30:57 +0100

    updated jest to support public-ip (conditional exports) - tests in green again

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
   transform: {
     '^.+\\.(jsx|js)?$': 'babel-jest',
-    '^.+\\.svg$': 'jest-svg-transformer',
+    '^.+\\.svg$': 'jest-transformer-svg',
   },
   transformIgnorePatterns: [
     '/node_modules/(?!public-ip|is-ip|ip-regex|indent-string|aggregate-error|clean-stack|escape-string-regexp).+\\.js$',
