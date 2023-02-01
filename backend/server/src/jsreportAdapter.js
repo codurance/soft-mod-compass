c19d6ceccf47cf7695731c33f612fc9fe69945c6
Sean Johnston <sean.johnston@codurance.com>
2023-02-01 14:52:10 +0000

    Update version of jsreport

--- backend/server/src/jsreportAdapter.js
+++ backend/server/src/jsreportAdapter.js
@@ -1,6 +1,7 @@
 const jsreport = require('jsreport');
 
 module.exports = async (jsReportTemplate, body) => {
+  await jsreport.init();
   return await jsreport.render({
     template: jsReportTemplate,
     data: body,
