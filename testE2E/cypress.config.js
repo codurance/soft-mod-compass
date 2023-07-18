dc701b7df27ce5145d8811e3dfe2c27be821354e
Víctor Carmona <vicarali@gmail.com>
2023-07-18 14:28:56 +0200

    Remove nodeVersion conf from cypress (deprecated)

--- testE2E/cypress.config.js
+++ testE2E/cypress.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
-  nodeVersion: 'system',
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
